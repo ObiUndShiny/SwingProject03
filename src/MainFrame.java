d4da8e92747a92e7b7e3861626dd4dde0defe421
Bernhard Scharrer <bernhard.scharrer@gmx.net>
2017-09-06 20:05:15 +0200

    implemented startup project

---
+++ src/MainFrame.java
@@ -0,0 +1,37 @@
+
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	public MainFrame() {
+		try {
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			FrameInit();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	/**
+	 * Component initialization.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	private void FrameInit() throws Exception {
+		contentPane = (JPanel) getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		super.setSize(new Dimension(300, 400));
+		super.setTitle("123");
+
+	}
+
+}
