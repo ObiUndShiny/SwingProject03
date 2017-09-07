1cb006bf70953ed00293ff3eacce4c0792b4a9e7
JulianScheinmayr <scheini48@gmail.com>
2017-09-07 11:40:12 +0200

    Listener ohne funktion erstellt und auf scrollbar geadded

--- src/AdjustMisteryPanel.java
+++ src/AdjustMisteryPanel.java
@@ -1,24 +1,50 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 
+
+
 public class AdjustMisteryPanel extends JPanel {
+	
+	private int red;
+	private int green;
+	private int blue;
 	
 	public AdjustMisteryPanel() {
 		super(new GridLayout(1, 3, 5, 10));
 		
-		JScrollBar scroll1 = new JScrollBar(JScrollBar.VERTICAL, 50, 10, 0, 110);
-		JScrollBar scroll2 = new JScrollBar(JScrollBar.VERTICAL, 50, 10, 0, 110);
-		JScrollBar scroll3 = new JScrollBar(JScrollBar.VERTICAL, 50, 10, 0, 110);
+		JScrollBar scroll1 = new JScrollBar(JScrollBar.VERTICAL, 0, 10, 0, 265);
+		JScrollBar scroll2 = new JScrollBar(JScrollBar.VERTICAL, 0, 10, 0, 265);
+		JScrollBar scroll3 = new JScrollBar(JScrollBar.VERTICAL, 0, 10, 0, 265);
 		
 		super.add(scroll1);
 		super.add(scroll2);
 		super.add(scroll3);
 		
+		scroll1.addAdjustmentListener(new ScrollbarListener());
+		scroll2.addAdjustmentListener(new ScrollbarListener());
+		scroll3.addAdjustmentListener(new ScrollbarListener());
+	}
+
+	private class ScrollbarListener implements AdjustmentListener{
+
+	@Override
+	public void adjustmentValueChanged(AdjustmentEvent e) {
+		
+		
+		
 	}
 	
+	
 }
+
+}
+
+
+
--- src/CodeMisteryPanel.java
+++ src/CodeMisteryPanel.java
@@ -1,5 +1,9 @@
 import javax.swing.JPanel;
 
+// ma kuntad sie bei AdjustMistery de zahlen vo 000 bis 255 ausgeben lossn
+// und don nimmst fias code mistery vom ersten de erste zahl, vom zweiten de zweite zahl und vom dritten de dritte zahl
+// Bsp: Adjustment: 000, 250, 123; Code Mistery: 053
+
 public class CodeMisteryPanel extends JPanel {
 	
 	public CodeMisteryPanel() {
